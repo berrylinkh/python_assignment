cde8c3ca741960f96fb39c5aeb1dec725990e92f
berrylinkh <berrylinkh@gmail>
2026-07-20 09:23:23 +0100

    added task

---
+++ Exercise 2/Circle_Area_Diameter_Circumference.py
@@ -0,0 +1,7 @@
+radius = 2
+pie_value = 3.14159
+diameter = 2* radius
+circle_area =2 * 3.14159 * radius
+circumference = 3.14159 * (radius *radius)
+print("Diameter: ",diameter, "\nCircle Area: ",circle_area, "\nCircumference: ",circumference)
+
---
+++ Exercise 2/grade.py
@@ -0,0 +1,3 @@
+grade = int(input('Enter an integer'))
+if grade >= 90 and grade <=100 :
+	print("Congratulations! your grade  of", grade, "earn you an A in this course")
